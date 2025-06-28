d7975391cff9e9c0a22dce9556f672e3abf8a95c
Sachin Sanap <sanapsachin2005@gmail.com>
2025-06-28 13:05:35 +0000

    Code challenge submission for question: 678 Valid Parenthesis String

---
+++ 678 Valid Parenthesis String/678valid-parenthesis-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> leftPar = new Stack<>();
+        Stack<Integer> stars = new Stack<>();
+
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if(ch == '(') {
+                leftPar.push(i);
+            } else if(ch == '*') {
+                stars.push(i);
+            } else {
+                if(!leftPar.isEmpty()) {
+                    leftPar.pop();
+                } else if(!stars.isEmpty()) {
+                    stars.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while(!leftPar.isEmpty() && !stars.isEmpty()) {
+            if(leftPar.pop() > stars.pop()) {
+                return false;     // * must be after '('
+            }
+        }
+
+        return leftPar.isEmpty();
+    }
+} 

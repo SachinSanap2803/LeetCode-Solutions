da5a12d9acf661c90ce70c03bc1cb8bfb750ede4
Sachin Sanap <sanapsachin2005@gmail.com>
2025-09-21 03:08:35 +0000

    Code challenge submission for question: 3689 Maximum Total Subarray Value I

---
+++ 3689 Maximum Total Subarray Value I/3689maximum-total-subarray-value-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int num : nums) {
+            if(num > max) {
+                max = num;
+            }
+            if(num < min) {
+                min = num;
+            }
+        }
+
+        long total = (long) max  - (long) min;
+        return total * k;
+    }
+} 

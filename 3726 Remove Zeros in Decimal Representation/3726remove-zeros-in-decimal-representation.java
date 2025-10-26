c4e5f89c50b7b78a86f94b23b90fe10f968b48cf
Sachin Sanap <sanapsachin2005@gmail.com>
2025-10-26 03:09:41 +0000

    Code challenge submission for question: 3726 Remove Zeros in Decimal Representation

---
+++ 3726 Remove Zeros in Decimal Representation/3726remove-zeros-in-decimal-representation.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long removeZeros(long n) {
+        String str = Long.toString(n);
+        String ans = "";
+
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) != '0') {
+                ans += str.charAt(i);
+            }
+        }
+
+        return Long.parseLong(ans);
+    }
+} 

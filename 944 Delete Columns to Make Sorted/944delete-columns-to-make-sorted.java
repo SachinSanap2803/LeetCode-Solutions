fcc858e7b235e9b0274afbfccd18361df9e4488e
Sachin Sanap <sanapsachin2005@gmail.com>
2025-12-20 06:16:46 +0000

    Code challenge submission for question: 944 Delete Columns to Make Sorted

---
+++ 944 Delete Columns to Make Sorted/944delete-columns-to-make-sorted.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int count = 0;
+        int rows = strs.length;
+        int cols = strs[0].length();
+
+        for(int col=0; col<cols; col++) {
+            for(int row=0; row<rows-1; row++) {
+                if(strs[row].charAt(col) > strs[row+1].charAt(col)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+} 

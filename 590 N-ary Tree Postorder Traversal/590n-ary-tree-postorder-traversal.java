dcb9485e25ad105111d2f732686bb946d4027cd0
Sachin Sanap <sanapsachin2005@gmail.com>
2026-01-30 11:37:45 +0000

    Code challenge submission for question: 590 N-ary Tree Postorder Traversal

---
+++ 590 N-ary Tree Postorder Traversal/590n-ary-tree-postorder-traversal.java
@@ -0,0 +1,62 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+// class Solution {
+
+//     public void func(Node root, List<Integer> list) {
+//         if(root == null) return;
+
+//         for(Node child : root.children) {
+//             func(child, list);
+//         }
+
+//         list.add(root.val);
+//     }
+
+//     public List<Integer> postorder(Node root) {
+//         List<Integer> list = new ArrayList<>();
+//         func(root, list);
+//         return list;
+//     }
+// } 
+
+
+
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+
+        while(!st.isEmpty()) {
+            Node top = st.pop();
+            list.add(top.val);
+
+            for(Node child : top.children) {
+                st.push(child);
+            }
+        }
+
+        Collections.reverse(list);
+        return list;
+    }
+} 

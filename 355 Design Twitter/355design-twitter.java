a5c79417e165edd1151f9e85630395499d0edef7
Sachin Sanap <sanapsachin2005@gmail.com>
2026-02-25 12:26:23 +0000

    Code challenge submission for question: 355 Design Twitter

---
+++ 355 Design Twitter/355design-twitter.java
@@ -0,0 +1,77 @@
+class Twitter {
+    // map to store user's tweets (timestamp, tweetId)
+    HashMap<Integer, List<int[]>> tweets;
+
+    // map to store who follows whom
+    HashMap<Integer, Set<Integer>> following;
+
+    // timer count to give each tweet a unique timestamp
+    int time;
+
+    public Twitter() {
+        tweets = new HashMap<>();
+        following = new HashMap<>();
+        time = 0;
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        tweets.putIfAbsent(userId, new ArrayList<>());
+        tweets.get(userId).add(new int[] {time++, tweetId});
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        // Add own tweets
+        if(tweets.containsKey(userId)) {
+            for(int[] tweet : tweets.get(userId)) {
+                pq.offer(tweet);
+                if(pq.size() > 10) {
+                    pq.poll();
+                }
+            }
+        }
+
+        // Add tweets from followed users
+        if(following.containsKey(userId)) {
+            for(int followee : following.get(userId)) {
+                if(tweets.containsKey(followee)) {
+                    for(int[] tweet : tweets.get(followee)) {
+                        pq.offer(tweet);
+                        if(pq.size() > 10) {
+                            pq.poll();
+                        }
+                    }
+                }
+            }
+        }
+
+        LinkedList<Integer> list = new LinkedList<>();
+        while(!pq.isEmpty()) {
+            list.addFirst(pq.poll()[1]);
+        }
+
+        return list;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        following.putIfAbsent(followerId, new HashSet<>());
+        following.get(followerId).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if(following.containsKey(followerId)) {
+            following.get(followerId).remove(followeeId);
+        }
+    }
+}
+
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */ 

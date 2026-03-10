c0c5c5382bbc0dcd1b06ae4594d87afd68526133
Sachin Sanap <sanapsachin2005@gmail.com>
2026-03-10 17:47:47 +0000

    Code challenge submission for question: 347 Top K Frequent Elements

---
+++ 347 Top K Frequent Elements/347top-k-frequent-elements.java
@@ -0,0 +1,59 @@
+// Brute force - O(nlogn)
+
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         int n = nums.length;
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int i=0; i<n; i++) {
+//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//         }
+
+//         List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+
+//         list.sort((a, b) -> b.getValue() - a.getValue());
+
+//         int[] ans = new int[k];
+        
+//         for(int i=0; i<k; i++) {
+//             ans[i] = list.get(i).getKey();
+//         }
+
+//         return ans;
+//     }
+// } 
+
+
+
+
+
+// Optimal Solution
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+
+        for(int num : map.keySet()) {
+            pq.offer(num);
+
+            if(pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] ans = new int[k];
+        
+        for(int i=0; i<k; i++) {
+            ans[i] = pq.poll();
+        }
+
+        return ans;
+    }
+} 

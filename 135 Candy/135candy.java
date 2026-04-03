75b7996b63a4a57abb0b9dbdfab31ac3bf3d3d2c
Sachin Sanap <sanapsachin2005@gmail.com>
2026-04-03 11:45:27 +0000

    Code challenge submission for question: 135 Candy

---
+++ 135 Candy/135candy.java
@@ -0,0 +1,73 @@
+// Brute force
+
+// class Solution {
+//     public int candy(int[] ratings) {
+//         int n = ratings.length;
+//         int[] candies = new int[n];
+//         Arrays.fill(candies, 1);
+
+//         for(int i=1; i<n; i++) {
+//             if(ratings[i] > ratings[i-1]) {
+//                 candies[i] = candies[i-1] + 1;
+//             }
+//         }
+
+//         for(int i=n-2; i>=0; i--) {
+//             if(ratings[i] > ratings[i+1]) {
+//                 candies[i] = Math.max(candies[i] ,candies[i+1] + 1);
+//             }
+//         }
+
+//         int ans = 0;
+
+//         for(int i=0; i<n; i++) {
+//             ans += candies[i];
+//         }
+
+//         return ans;
+//     }
+// } 
+
+
+
+
+
+
+// Optimal Solution - Slope based approach
+
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int candies = n;
+
+        int i = 1;
+
+        while(i < n) {
+            // flat slope
+            if(ratings[i] == ratings[i-1]) {
+                i++;
+                continue;
+            }
+
+            // Increasing slope
+            int peak = 0;
+            while(i < n && ratings[i] > ratings[i-1]) {
+                peak += 1;
+                candies += peak;
+                i++;
+            }
+
+            // Decreasing slope
+            int down = 0;
+            while(i < n && ratings[i] < ratings[i-1]) {
+                down += 1;
+                candies += down;
+                i++;
+            }
+
+            candies -= Math.min(peak, down);
+        }
+
+        return candies;
+    }
+} 

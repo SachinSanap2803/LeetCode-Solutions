3a0f08e43ed3446233d2b21a9138c28de884de4b
Sachin Sanap <sanapsachin2005@gmail.com>
2026-04-09 16:29:17 +0000

    Code challenge submission for question: 739 Daily Temperatures

---
+++ 739 Daily Temperatures/739daily-temperatures.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+
+        Stack<int[]> st = new Stack<>();
+
+        for(int i=n-1; i>=0; i--) {
+            while(!st.isEmpty() && st.peek()[0] <= temperatures[i]) {
+                st.pop();
+            }
+            
+            res[i] = st.isEmpty() ? 0 : st.peek()[1] - i;
+            st.push(new int[]{temperatures[i], i});
+        }
+
+        return res;
+    }
+} 

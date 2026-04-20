bf5b400c556f4b039d1315259b337d7c483b23b5
Sachin Sanap <sanapsachin2005@gmail.com>
2026-04-20 16:26:22 +0000

    Code challenge submission for question: 2078 Two Furthest Houses With Different Colors

---
+++ 2078 Two Furthest Houses With Different Colors/2078two-furthest-houses-with-different-colors.java
@@ -0,0 +1,47 @@
+// Brute force
+
+// class Solution {
+//     public int maxDistance(int[] colors) {
+//         int n = colors.length;
+//         int maxi = 0;
+
+//         for(int i=0; i<n-1; i++) {
+//             for(int j=i+1; j<n; j++) {
+//                 if(colors[i] != colors[j]) {
+//                     maxi = Math.max(maxi, j-i);
+//                 }
+//             }
+//         }
+
+//         return maxi;
+//     }
+// }
+
+
+
+
+
+// Optimal Solution
+
+class Solution {
+    public int maxDistance(int[] colors) {
+        int n = colors.length;
+        int maxi = 0;
+
+        for(int j=n-1; j>=0; j--) {
+            if(colors[j] != colors[0]) {
+                maxi = Math.max(maxi, j);
+                break;
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            if(colors[i] != colors[n-1]) {
+                maxi = Math.max(maxi, n-i-1);
+                break;
+            }
+        }
+
+        return maxi;
+    }
+} 

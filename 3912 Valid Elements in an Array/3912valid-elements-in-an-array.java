0f5069ed1918285856d67bf159bf3f354b28f034
Sachin Sanap <sanapsachin2005@gmail.com>
2026-04-26 02:39:36 +0000

    Code challenge submission for question: 3912 Valid Elements in an Array

---
+++ 3912 Valid Elements in an Array/3912valid-elements-in-an-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Integer> findValidElements(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        
+        if(n == 1) {
+            res.add(nums[0]);
+            return res;
+        }
+
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMax[0] = Integer.MIN_VALUE;
+        for(int i=1; i<n; i++) {
+            leftMax[i] = Math.max(leftMax[i-1], nums[i-1]);
+        }
+
+        rightMax[n-1] = Integer.MIN_VALUE;
+        for(int i=n-2; i>=0; i--) {
+            rightMax[i] = Math.max(rightMax[i+1], nums[i+1]);
+        }
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] > leftMax[i] || nums[i] > rightMax[i]) {
+                res.add(nums[i]);
+            }
+        }
+
+        return res;
+    }
+} 

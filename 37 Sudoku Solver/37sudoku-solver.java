49a8e1e5d10bfc0467d067b11df29e08d15f7e5a
Sachin Sanap <sanapsachin2005@gmail.com>
2026-04-30 18:01:28 +0000

    Code challenge submission for question: 37 Sudoku Solver

---
+++ 37 Sudoku Solver/37sudoku-solver.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        int row = board.length;
+        int col = board[0].length;
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(board[i][j] == '.') {
+                    for(char ch='1'; ch<='9'; ch++) {
+                        if(isValid(board, i, j, ch)) {
+                            board[i][j] = ch;
+
+                            if(solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char ch) {
+        for(int i=0; i<9; i++) {
+            if(board[i][col] == ch) {
+                return false;
+            }
+
+            if(board[row][i] == ch) {
+                return false;
+            }
+
+            if(board[3 * (row/3) + (i/3)][3 * (col/3) + (i%3)] == ch) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+} 

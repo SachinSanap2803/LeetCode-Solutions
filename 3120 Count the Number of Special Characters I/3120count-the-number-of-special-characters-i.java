75b994ba9f6c82cf670083e606e5772e4f7f2bc9
Sachin Sanap <sanapsachin2005@gmail.com>
2026-05-26 05:46:18 +0000

    Code challenge submission for question: 3120 Count the Number of Special Characters I

---
+++ 3120 Count the Number of Special Characters I/3120count-the-number-of-special-characters-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int count = 0;
+
+        HashSet<Character> set = new HashSet<>();
+
+        for(char ch : word.toCharArray()) {
+            set.add(ch);
+        }
+
+        for(char ch='a'; ch<='z'; ch++) {
+            if(set.contains(ch) && set.contains((char)(ch-32))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+} 

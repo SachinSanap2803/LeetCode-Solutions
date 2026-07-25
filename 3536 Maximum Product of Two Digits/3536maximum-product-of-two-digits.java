9e0a0faf87f8476699100e6f2e10ce960bade631
Sachin Sanap <sanapsachin2005@gmail.com>
2026-07-25 12:48:03 +0000

    Code challenge submission for question: 3536 Maximum Product of Two Digits

---
+++ 3536 Maximum Product of Two Digits/3536maximum-product-of-two-digits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(int n) {
+        int max = 0;
+        int secMax = 0;
+
+        while(n > 0) {
+            int digit = n % 10;
+
+            if(digit > max) {
+                secMax = max;
+                max = digit;
+            } else if(digit > secMax) {
+                secMax = digit;
+            }
+
+            n /= 10;
+        }
+
+        return max * secMax;
+    }
+} 

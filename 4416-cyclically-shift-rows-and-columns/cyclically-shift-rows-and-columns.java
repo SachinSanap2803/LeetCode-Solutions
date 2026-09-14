7c4a7f2607a8f8d6fe1cb62be3e0e11cd9de51cf
Sachin Sanap <156282569+SachinSanap2803@users.noreply.github.com>
2026-09-14 14:42:05 +0530

    Time: 1 ms (100.00%) | Memory: 47.3 MB (50.00%) - LeetSync

---
+++ 4416-cyclically-shift-rows-and-columns/cyclically-shift-rows-and-columns.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] cyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift) {
+        int[][] ans = new int[n][n];
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                int nc = (j - rowShift[i] + n) % n;
+                int nr = (i - colShift[nc] + n) % n;
+
+                ans[nr][nc] = grid[i][j];
+            }
+        }
+
+        return ans;
+    }
+} 
